bc7f20cd14bdb22e8ad052fe72d3b40de3d22257
scooter7 <scooter.vineburgh@gmail.com>
2024-07-26 11:24:13 -0500

    Update aisql.py

--- aisql.py
+++ aisql.py
@@ -1,13 +1,11 @@
 import streamlit as st
-import pandas as pd
 
 # Initialize connection.
-# Uses st.connection's secrets management.
 conn = st.connection('mysql', type='sql')
 
 # Perform query.
-query = 'SELECT * FROM table1;'  # Change 'your_table' to your actual table name
-df = conn.query(query, ttl=600)
+df = conn.query('SELECT * from table1;', ttl=600)
 
 # Print results.
-st.write(df)
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.age}:")
